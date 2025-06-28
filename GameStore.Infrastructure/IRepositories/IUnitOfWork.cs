d3bd81d037a6deaa3c0c2508232bb1a8b4d7cf30
Hayotbahrom Nurdinov <khayotbakhrom_nurdinov@epam.com>
2025-06-28 10:16:31 +0500

    Implemented UnitOfWork

---
+++ GameStore.Infrastructure/IRepositories/IUnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="IUnitOfWork.cs" company="GameStore">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace GameStore.Infrastructure.IRepositories
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Represents a unit of work interface for the Game Store application.
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        /// <summary>
+        /// Gets the repository for managing games.
+        /// </summary>
+        IGameRepository GameRepository { get; }
+
+        /// <summary>
+        /// Gets the repository for managing platforms.
+        /// </summary>
+        IPlatformRepository PlatformRepository { get; }
+
+        /// <summary>
+        /// Gets the repository for managing genres.
+        /// </summary>
+        IGenreRepository GenreRepository { get; }
+
+        /// <summary>
+        /// Asynchronously saves changes to the database.
+        /// </summary>
+        /// <returns>An integer representing the number of state entries written to database. </returns>
+        Task<int> SaveChangesAsync();
+    }
+}
