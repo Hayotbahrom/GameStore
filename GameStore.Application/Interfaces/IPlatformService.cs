24007e7ebb0ad59170c6575c8213351cf11f1680
Hayotbahrom Nurdinov <khayotbakhrom_nurdinov@epam.com>
2025-06-29 15:31:37 +0500

    Implemented PlatformService Added Platform For Creation, Update, Result DTOs

---
+++ GameStore.Application/Interfaces/IPlatformService.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="IPlatformService.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace GameStore.Application.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using GameStore.Application.DTOs.Platforms;
+
+    /// <summary>
+    /// Provides an interface for platform services.
+    /// </summary>
+    public interface IPlatformService
+    {
+        /// <summary>
+        /// Gets all platforms.
+        /// </summary>
+        /// <returns>A list of platforms.</returns>
+        Task<IEnumerable<PlatformForResultDto>> GetAllPlatformsAsync();
+
+        /// <summary>
+        /// Gets a platform by its ID.
+        /// </summary>
+        /// <param name="id">The unique identifier of the platform.</param>
+        /// <returns>The platform with the specified ID.</returns>
+        Task<PlatformForResultDto?> GetPlatformByIdAsync(Guid id);
+
+        /// <summary>
+        /// Gets platforms associated with a specific game by its key.
+        /// </summary>
+        /// <param name="gameKey"></param>
+        /// <returns>List of platforms</returns>
+        Task<IEnumerable<PlatformForResultDto>> GetByGameKeyAsync(string gameKey);
+
+        /// <summary>
+        /// Creates a new platform.
+        /// </summary>
+        /// <param name="platform">The platform to create.</param>
+        /// <returns>The created platform.</returns>
+        Task<PlatformForResultDto> CreatePlatformAsync(PlatformForCreationDto platform);
+
+        /// <summary>
+        /// Updates an existing platform.
+        /// </summary>
+        /// <param name="platform">The platform to update.</param>
+        Task<bool> UpdatePlatformAsync(PlatformForUpdateDto platform);
+
+        /// <summary>
+        /// Deletes a platform by its ID.
+        /// </summary>
+        /// <param name="id">The unique identifier of the platform to delete.</param>
+        Task<bool> DeletePlatformAsync(Guid id);
+    }
+}
---
+++ GameStore.Application/Services/PlatformService.cs
@@ -0,0 +1,96 @@
+﻿// <copyright file="PlatformService.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace GameStore.Application.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using AutoMapper;
+    using GameStore.Application.DTOs.Platforms;
+    using GameStore.Application.Interfaces;
+    using GameStore.Domain.Entities;
+    using GameStore.Infrastructure.IRepositories;
+    using Microsoft.EntityFrameworkCore;
+    public class PlatformService : IPlatformService
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+
+        public PlatformService(
+            IUnitOfWork unitOfWork,
+            IMapper mapper)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+        }
+        /// <inheritdoc />
+        public async Task<PlatformForResultDto> CreatePlatformAsync(PlatformForCreationDto dto)
+        {
+            var platform = new Platform
+            {
+                Id = Guid.NewGuid(),
+                Type = dto.Type
+            };
+
+            await this.unitOfWork.Platforms.AddAsync(platform);
+            await this.unitOfWork.SaveChangesAsync();
+            return this.mapper.Map<PlatformForResultDto>(platform);
+        }
+
+        /// <inheritdoc />
+        public async Task<bool> DeletePlatformAsync(Guid id)
+        {
+            var platform = await this.unitOfWork.Platforms.FindByIdAsync(id);
+            if (platform == null)
+            {
+                return false;
+            }
+
+            this.unitOfWork.Platforms.Delete(platform);
+            await this.unitOfWork.SaveChangesAsync();
+            return true;
+        }
+
+        /// <inheritdoc />
+        public async Task<IEnumerable<PlatformForResultDto>> GetAllPlatformsAsync()
+        {
+            var platforms = await this.unitOfWork.Platforms.GetAll().ToListAsync();
+            return this.mapper.Map<IEnumerable<PlatformForResultDto>>(platforms);
+        }
+
+        /// <inheritdoc />
+        public async Task<IEnumerable<PlatformForResultDto>> GetByGameKeyAsync(string gameKey)
+        {
+            var platforms = await this.unitOfWork.Platforms.GetByGameKeyAsync(gameKey);
+            return this.mapper.Map<IEnumerable<PlatformForResultDto>>(platforms);
+        }
+
+        /// <inheritdoc />
+        public async Task<PlatformForResultDto?> GetPlatformByIdAsync(Guid id)
+        {
+            var platform = await this.unitOfWork.Platforms.FindByIdAsync(id);
+            if (platform == null)
+            {
+                return null;
+            }
+            return this.mapper.Map<PlatformForResultDto>(platform);
+        }
+
+        /// <inheritdoc />
+        public async Task<bool> UpdatePlatformAsync(PlatformForUpdateDto platform)
+        {
+            var existingPlatform = await this.unitOfWork.Platforms.FindByIdAsync(platform.Id);
+            if (existingPlatform == null)
+            {
+                return false;
+            }
+
+            existingPlatform.Type = platform.Type;
+            await this.unitOfWork.SaveChangesAsync();
+            return true;
+        }
+    }
+}
