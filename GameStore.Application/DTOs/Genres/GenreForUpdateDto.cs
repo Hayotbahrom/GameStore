60494eb4ea32863d8df50ea2a88ac2651a07945c
Hayotbahrom Nurdinov <khayotbakhrom_nurdinov@epam.com>
2025-06-29 15:30:39 +0500

    Implemented GenreService Added GenreFor Creation, Update, Result DTOs

---
+++ GameStore.Application/DTOs/Genres/GenreForUpdateDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Application.DTOs.Genres
+{
+    public class GenreForUpdateDto
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for the genre.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the genre.
+        /// </summary>
+        [Required]
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Id of the parent genre, if this genre is a sub-genre.
+        /// </summary>
+        public Guid? ParentGenreId { get; set; }
+    }
+}
---
+++ GameStore.Application/Interfaces/IGenreService.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="IGenreService.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace GameStore.Application.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using GameStore.Application.DTOs.Genres;
+
+    /// <summary>
+    /// Provides an interface for genre services.
+    /// </summary>
+    public interface IGenreService
+    {
+        Task<IEnumerable<GenreForResultDto>> GetAllAsync();
+        Task<GenreForResultDto?> GetByIdAsync(Guid id);
+        Task<IEnumerable<GenreForResultDto>> GetByParentIdAsync(Guid parentId);
+        Task<IEnumerable<GenreForResultDto>> GetByGameKeyAsync(string gameKey);
+        Task<Guid> AddAsync(GenreForCreationDto dto);
+        Task<bool> UpdateAsync(GenreForUpdateDto dto);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
